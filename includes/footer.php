49555bff159c60159c9c90c2bb594de2f3f839bb
Valmir Paiva Stachin <valmir@unimake.com.br>
2026-05-21 19:17:49 -0300

    feat: Adiciona footer.php inicial

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+  <!-- <div class="container text-center">
+    <p class="text-muted small">© 2026 - Todos os direitos reservados</p>
+  </div> -->
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
