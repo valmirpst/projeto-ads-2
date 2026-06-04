f4fafd9b981620824b02da6df10d39c7ec69184d
Valmir Paiva Stachin <valmir@unimake.com.br>
2026-06-04 14:24:12 -0300

    feat: Adiciona favicon.ico e configura no header.php

--- includes/header.php
+++ includes/header.php
@@ -7,7 +7,6 @@ $navLinks = [
 ];
 ?>
 
-
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -15,6 +14,8 @@ $navLinks = [
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>ManuMake</title>
+
+  <link rel="icon" type="image/x-icon" href="<?= $baseUrl ?>/favicon.ico">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
